5c613ab1541b711acd4bea92454832b5b8cd58b2
NorbertPap <garrote713@gmail.com>
2019-02-08 10:59:55 +0100

    Raise if pair in hands

--- Player.js
+++ Player.js
@@ -6,6 +6,21 @@ class Player {
   static betRequest(gameState, bet) {
     console.log(gameState.players[2].stack);
     bet(gameState.players[2].stack);
+
+    let me = gameState.players[2];
+    let myCards = me.hole_cards;
+    let communityCards = gameState.community_cards;
+    let myBet = 0;
+
+    if(myCards[0].rank === myCards[1].rank){
+      if(gameState.minimum_raise * 1.5 > me.stack){
+        bet(me.stack);
+      } else {
+        bet(gameState.minimum_raise*1.5);
+      }
+    } else {
+      bet(10);
+    }
   }
 
   static showdown(gameState) {
